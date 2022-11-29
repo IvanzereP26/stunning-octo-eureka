8a4aeafbc0aaea2463020da13fc465cc9494423a
IvazereP26 <ivanmartinperez2601@gmail.com>
2022-11-29 13:17:53 +0100

    Añadiendo cambios en el archivo y modificando el README.md

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Iván Martín Pérez");
+    System.out.println("Martes 29 de Noviembre de 2022");
+    System.out.println("Mi lugar de residencia es San Andrés");
   }
   
 }
